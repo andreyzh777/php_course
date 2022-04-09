565a2ef52e8fcb4ea4fea2fcf1d82394e136584b
Dzianis Kotau <me@dzianiskotau.com>
2022-04-09 17:14:42 +0400

    First example

---
+++ src/Task1/MyTernary.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Tests whether a number is greater than 30, 20 or 10 using ternary operator.
+ *
+ * @param int $n Test number
+ *
+ * @return string
+ * @author Dzianis Kotau <me@dzianiskotau.com>
+ *
+ */
+function myTernary(int $n): string
+{
+    $r = $n > 30
+        ? 'greater than 30'
+        : ($n > 20 ? 'greater than 20'
+            : ($n > 10 ? 'greater than 10' : 'less or equal to 10'));
+
+    return ($n . ' ' . $r);
+}
+
+echo myTernary(25) . PHP_EOL;
