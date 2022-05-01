d1aa968bdb93d1204a08bb3706444568d670dba4
-g <andrey.zhartun@gmail.com>
2022-05-02 00:08:23 +0300

    01.05 change

--- src/Task17/task17.php
+++ src/Task17/task17.php
@@ -1,19 +1,18 @@
 <?php
 
-function outputArray (int $n): array
+function outputArray(int $n): array
 {
     $a = [];
     do
     {
-        array_push($a, $n);
-        if ($n %2 == 0) {
+        $a[] = $n;
+        if ($n % 2 == 0) {
             $n /= 2;
-        }
-        else {
+        } else {
             $n = 3 * $n + 1;
         }
-    } while ($n>1);
-    array_push($a, $n);
+    } while ($n > 1);
+    $a[] = $n;
 
     return $a;
 }
