c94fd701588e7b1516dd361f90bf9fa2dd1b27a4
-g <andrey.zhartun@gmail.com>
2022-05-02 00:20:00 +0300

    01.05 change

--- src/Task16/task16.php
+++ src/Task16/task16.php
@@ -1,16 +1,15 @@
 <?php
-function find (array $a, int $n): array
+function find(array $a, int $n): array
 {
     $arrayOut = [];
-    $kollElement = count ($a) - 2;
-    for ($i = 0; $i < $kollElement; $i++)
+    $amountElement = count ($a) - 2;
+    for ($i = 0; $i < $amountElement; $i++)
     {
         $sum = $a[$i] + $a[$i + 1] + $a[$i + 2];
-        if ($sum == $n) {
-            $str = strval($a[$i]) . " + " . strval($a[$i + 1]) ." + " . strval($a[$i + 2]) . " = " . strval($sum);
-            array_push($arrayOut, $str);
+        if ($sum === $n) {
+            $str = $a[$i] . " + " . $a[$i + 1] ." + " . $a[$i + 2] . " = " . $sum;
+            $arrayOut[] = $str;
         }
-
     }
 
     return $arrayOut;
